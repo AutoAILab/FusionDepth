f6cf2899d155d651c5f340007cb9898771d7ffeb
Ziyue Feng <zfeng@clemson.edu>
2022-04-26 15:08:24 -0400

    Update prepare_4beam_data_for_prediction.sh

--- prepare_4beam_data_for_prediction.sh
+++ prepare_4beam_data_for_prediction.sh
@@ -5,6 +5,6 @@ python sparsify.py --W 1024 --H 64 --line_spec 2 7 12 16 --split_file ../splits/
 python sparsify.py --W 1024 --H 64 --line_spec 2 7 12 16 --split_file ../splits/eigen_full/val_files.txt
 python sparsify.py --W 1024 --H 64 --line_spec 2 7 12 16 --split_file ../splits/eigen/test_files.txt
 cd ..
-python export_gt_depth.py --split eigen
-python export_gt_depth.py --split 4beam
+python export_gt_depth.py --split eigen # only run these two line if you already have "kitti_data" folder and data
+python export_gt_depth.py --split 4beam # only run these two line if you already have "kitti_data" folder and data
 python gen2channel.py regen 4beam
